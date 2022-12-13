0dfd827856d3a526ff8fa57e7e953309a44f3e32
Alex Chemus <aachemus@gmail.com>
2022-12-13 11:01:43 +0500

    add redux logic

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,19 +1,56 @@
-export function* loadProductsSaga(clear?: boolean) {
+import {
+  select, takeLeading, call, put, delay, putResolve
+} from "redux-saga/effects"
+import {
+  GET_ALL_FILTERS, GET_PRODUCTS, UPDATE_FILTERS
+} from "../constants"
+import type { PayloadAction } from "@reduxjs/toolkit"
+import { State } from "../store"
+import { getFilters, getProducts } from "@/api"
+import { Product, addProducts, incrementChunks, clearProducts, clearChunks } from "../reducers/productsReducer"
+import { addFilters, removeFilters, setAllFilters } from "../reducers/filtersReducer"
 
+export function* getProductsSaga() {
+  const chunksCounter: number = yield select((state: State) => state.products.chunksCounter)
+  const chunkLength: number = yield select((state: State) => state.products.chunkLength)
+  const filters: string[] = yield select((state: State) => state.filters.filters)
+  
+  const products: Product[] = yield call(getProducts, {
+    filters,
+    limit: chunkLength * (chunksCounter+1)
+  })
+
+  yield put(addProducts(products))
+  yield put(incrementChunks())
 }
 
-export function* loadFiltersSaga() {
-
+export function* getAllFiltersSaga() {
+  const filters: string[] = yield call(getFilters)
+  yield put(setAllFilters(filters))
 }
 
-export function* updateFiltersSaga() {
+type Payload = {
+  remove: boolean,
+  filters: string[]
+}
 
+export function* updateFiltersSaga(action: PayloadAction<Payload>) {
+  const { remove, filters } = action.payload
+  if (!remove) yield putResolve(addFilters(filters))
+  else yield putResolve(removeFilters(filters))
+
+  yield putResolve(clearProducts())
+  yield putResolve(clearChunks())
+
+  yield getProductsSaga()
 }
 
 export function* watchSaga() {
-
+  yield takeLeading(GET_PRODUCTS, getProductsSaga)
+  yield takeLeading(GET_ALL_FILTERS, getAllFiltersSaga)
+  yield takeLeading(UPDATE_FILTERS, updateFiltersSaga)
 }
 
 export default function* rootSaga() {
-
+  yield watchSaga()
 }
--- src/redux/tests/updateFiltersSaga.test.ts
+++ src/redux/tests/updateFiltersSaga.test.ts
@@ -1,35 +1,48 @@
-import { updateFiltersSaga, loadProductsSaga } from "../sagas"
-import { call } from 'redux-saga/effects'
+import { updateFiltersSaga, getProductsSaga } from "../sagas"
+import { call, putResolve } from 'redux-saga/effects'
 import { addFilters, removeFilters } from "../reducers/filtersReducer"
 import { clearChunks, clearProducts } from "../reducers/productsReducer"
+import { UPDATE_FILTERS } from "../constants"
 
 describe('updateFiltersSaga', () => {
   it('should clear products and chunks and add new filters', () => {
-    const gen = updateFiltersSaga()
-    const newFilters = ['electronics']
+    const payload = {
+      remove: false,
+      filters: ['electronics']
+    }
+    const gen = updateFiltersSaga({
+      type: UPDATE_FILTERS,
+      payload
+    })
 
-    expect(gen.next().value).toEqual(call(addFilters, newFilters))
+    expect(gen.next().value).toEqual(putResolve(addFilters(payload.filters)))
 
-    expect(gen.next().value).toEqual(call(clearProducts))
+    expect(gen.next().value).toEqual(putResolve(clearProducts()))
 
-    expect(gen.next().value).toEqual(call(clearChunks))
+    expect(gen.next().value).toEqual(putResolve(clearChunks()))
 
-    expect(gen.next().value).toEqual(loadProductsSaga(true))
-
+    expect(JSON.stringify(gen.next().value)).toEqual(JSON.stringify(getProductsSaga()))
+    
     expect(gen.next().done).toEqual(true)
   })
 
   it('should clear products and chunks and remove new filters', () => {
-    const gen = updateFiltersSaga()
-    const oldFilters = ['electronics']
+    const payload = {
+      remove: true,
+      filters: ['electronics']
+    }
+    const gen = updateFiltersSaga({
+      type: UPDATE_FILTERS,
+      payload
+    })
 
-    expect(gen.next().value).toEqual(call(removeFilters, oldFilters))
+    expect(gen.next().value).toEqual(putResolve(removeFilters(payload.filters)))
 
-    expect(gen.next().value).toEqual(call(clearProducts))
+    expect(gen.next().value).toEqual(putResolve(clearProducts()))
 
-    expect(gen.next().value).toEqual(call(clearChunks))
+    expect(gen.next().value).toEqual(putResolve(clearChunks()))
 
-    expect(gen.next().value).toEqual(loadProductsSaga(true))
+    expect(JSON.stringify(gen.next().value)).toEqual(JSON.stringify(getProductsSaga()))
 
     expect(gen.next().done).toEqual(true)
   })
