fd1ac001cda2961b779f9076aa5487761d566eb9
Alex Chemus <aachemus@gmail.com>
2022-12-14 11:53:29 +0500

    added feed

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,17 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { Product } from '@/redux/reducers/productsReducer'
+import classes from './Card.module.scss'
 
-const Card = () => {}
+const Card: FC<{ product: Product }> = ({ product }) => {
+  return (
+    <article className={classes.Card}>
+      <img src={product.image} alt={product.title} />
+      <div className={classes.Wrapper}>
+        <h6 title={product.title}>{product.title}</h6>
+        <small>{product.price}$</small>
+      </div>
+    </article>
+  )
+}
 
 export default Card
