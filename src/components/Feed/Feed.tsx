880d2de14bf1b68558b95d2d72e4b2b97b16accd
Alex Chemus <aachemus@gmail.com>
2022-12-16 12:24:02 +0500

    added intersection observer

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { State } from '@/redux/store'
 import { getProducts } from '@/redux/sagas/actionCreators'
@@ -10,8 +10,22 @@ const Feed: FC = () => {
   const products = useSelector((state: State) => state.products.products)
   const dispatch = useDispatch()
 
+  const observable = useRef<null | HTMLDivElement>(null)
+
   useEffect(() => {
     dispatch(getProducts())
+
+    if (!observable.current) return
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        console.log('intersects')
+        dispatch(getProducts())
+      }
+    }, {
+      root: null,
+      rootMargin: '50px',
+    })
+    observer.observe(observable.current)
   }, [])
 
   if (products.length === 0) return (
@@ -20,13 +34,14 @@ const Feed: FC = () => {
     </section>
   )
 
-  return (
+  return (<>
     <section className={classes.Feed}>
       {products.map((product, i) => {
         return <Card product={product} key={i} />
       })}
     </section>
-  )
+    <div ref={observable} />
+  </>)
 }
 
 export default Feed
